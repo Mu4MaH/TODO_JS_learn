8bba4e33409b1a4f2395aaaed8ee638db71f8072
lushchikhin <3232qwr>
2019-03-06 18:30:19 +0300

    LocalStorage add

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,20 @@ class Task {
     }
 }
 
+window.onload = function () {
+    const tabbl = document.getElementById('tasksTable');
+    const trs = tabbl.children;
+    let num = 1;
+    let storage = localStorage;
+    for (let key in storage) {
+        const newTr = document.createElement('tr');
+        newTr.innerHTML =`<td><center>${num++}</center></td><td>  ${key}   </td><td>  ${storage.getItem(key)}  </td><td></td>`;
+        tabbl.appendChild(newTr, trs[(trs.length -1 )]);
+    }
+}
+
+
+
 function createTask() {
     if (document.getElementById("newTaskName").value == "") return;
     const taskName = document.getElementById('newTaskName').value;
@@ -15,6 +29,7 @@ function createTask() {
     const trs = tabbl.children;
     let num = trs.length;
     const newTr = document.createElement('tr');
+    localStorage.setItem(taskName,statusVal);
     newTr.innerHTML =`<td><center>${num++}</center></td><td>  ${taskName}   </td><td>  ${statusVal}  </td><td></td>`;
     tabbl.appendChild(newTr, trs[(trs.length -1 )]);
     document.getElementById("newTaskName").value = "";
