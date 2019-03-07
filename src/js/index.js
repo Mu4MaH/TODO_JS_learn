bd9846914b357d4f5d348a1f10ae32ed8bf07c15
lushchikhin <3232qwr>
2019-03-07 17:58:28 +0300

    Modification for tasks added, but not working.

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 'use strict';
+const storage = localStorage;
+let statusChangers;
 class Task {
     constructor(name, status) {
         this.name = name;
@@ -10,15 +12,20 @@ window.onload = function () {
     const tabbl = document.getElementById('tasksTable');
     const trs = tabbl.children;
     let num = 1;
-    let storage = localStorage;
     for (let key in storage) {
-        const newTr = document.createElement('tr');
-        newTr.innerHTML =`<td><center>${num++}</center></td><td>  ${key}   </td><td>  ${storage.getItem(key)}  </td><td></td>`;
-        tabbl.appendChild(newTr, trs[(trs.length -1 )]);
+        let newTr = document.createElement('tr');
+        let item = storage.getItem(key)
+        if (item && key != 'randid') {
+                        newTr.innerHTML = `<td><center>${num++}</center></td><td>  ${key}   </td><td class="currStatus">  ${item} </td><td><select class="statusChanger"> <option value="Запланировано">Запланировано</option><option value="Выполнено">Выполнено</option> <option value="Неплохо">Неплохо бы</option> </select></td>`;
+            tabbl.appendChild(newTr, trs[(trs.length - 1)]);
+        }
     }
+    statusChangers = document.getElementsByClassName("statusChanger");
 }
 
-
+function changeStatus(e) {
+alert('sdf');
+}
 
 function createTask() {
     if (document.getElementById("newTaskName").value == "") return;
@@ -29,17 +36,32 @@ function createTask() {
     const trs = tabbl.children;
     let num = trs.length;
     const newTr = document.createElement('tr');
-    localStorage.setItem(taskName,statusVal);
-    newTr.innerHTML =`<td><center>${num++}</center></td><td>  ${taskName}   </td><td>  ${statusVal}  </td><td></td>`;
+    storage.setItem(taskName,statusVal);
+    newTr.innerHTML =`<td><center>${num++}</center></td><td>  ${taskName}   </td><td>  ${statusVal}  </td><td><select onchange=""> <option value="Запланировано">Запланировано</option> `+
+        +`<option value="Выполнено">Выполнено</option>` +
+        +`<option value="Неплохо">Неплохо бы</option> </select></td>`;
     tabbl.appendChild(newTr, trs[(trs.length -1 )]);
     document.getElementById("newTaskName").value = "";
 }
 
-let btn = document.getElementById("newTaskButton");
-    btn.addEventListener("click", createTask);
-
-
-function sortByStatus() {
-
-
+function clearTasks() {
+    if (confirm("Очистить весь список? Вы уверены?")) {
+        storage.clear();
+        location.reload();
+    }
 }
+
+const createBtn = document.getElementById("newTaskButton");
+createBtn.addEventListener("click", createTask);
+const clearBtn = document.getElementById("clearBtn");
+clearBtn.addEventListener("click", clearTasks);
+for (let i = 0; i < statusChangers.length; i++) {
+    statusChangers(i).addEventListener("change",changeStatus);
+}
+
+
+//
+// function sortByStatus() {
+// let sortArr =
+//
+// }
